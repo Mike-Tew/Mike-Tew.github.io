77287954d56f4358c52b0541d79d902e2ce796a6
Michael Tew <mike.tew@gmail.com>
2019-05-26 12:31:32 -0700

    Uploaded Tic-tac-toe game

--- scripts/bmi-calculator.js
+++ scripts/bmi-calculator.js
@@ -1,33 +1,34 @@
+const $ = window.$;
 $(function () {
-    $("#navbar").load("navbar.html");
+    $('#navbar').load('navbar.html');
 });
 
-/* 
+/*
 A function that calculates a person's BMI and displays it.
 */
 function calculate() {
-    let weight = +(document.getElementById("weight").value);
-    let feet = +(document.getElementById("feet").value);
-    let inches = +(document.getElementById("inches").value);
+    let weight = +(document.getElementById('weight').value);
+    let feet = +(document.getElementById('feet').value);
+    let inches = +(document.getElementById('inches').value);
     let height = feet * 12 + inches;
     let bmi = (weight * 703) / (Math.pow(height, 2));
-    document.getElementById("bmi").innerHTML = bmi.toFixed(1);
-    let bmiText = document.getElementById("bmi-text");
+    document.getElementById('bmi').innerHTML = bmi.toFixed(1);
+    let bmiText = document.getElementById('bmi-text');
     if (bmi < 16) {
-        bmiText.innerHTML = "Severe Thinness";
+        bmiText.innerHTML = 'Severe Thinness';
     } else if (bmi < 17) {
-        bmiText.innerHTML = "Moderate Thinness";
+        bmiText.innerHTML = 'Moderate Thinness';
     } else if (bmi < 18.5) {
-        bmiText.innerHTML = "Mild Thinness";
+        bmiText.innerHTML = 'Mild Thinness';
     } else if (bmi < 25) {
-        bmiText.innerHTML = "Normal";
+        bmiText.innerHTML = 'Normal';
     } else if (bmi < 30) {
-        bmiText.innerHTML = "Overweight";
+        bmiText.innerHTML = 'Overweight';
     } else if (bmi < 35) {
-        bmiText.innerHTML = "Obese Class I";
+        bmiText.innerHTML = 'Obese Class I';
     } else if (bmi < 40) {
-        bmiText.innerHTML = "Obese Class II";
+        bmiText.innerHTML = 'Obese Class II';
     } else {
-        bmiText.innerHTML = "Obese Class III";
+        bmiText.innerHTML = 'Obese Class III';
     }
-}
+};
---
+++ tic-tac-toe/tic-tac-toe.js
@@ -0,0 +1,55 @@
+/* eslint-disable indent */
+let playerOne = true;
+const s1 = document.getElementById('square-1');
+const s2 = document.getElementById('square-2');
+const s3 = document.getElementById('square-3');
+const s4 = document.getElementById('square-4');
+const s5 = document.getElementById('square-5');
+const s6 = document.getElementById('square-6');
+const s7 = document.getElementById('square-7');
+const s8 = document.getElementById('square-8');
+const s9 = document.getElementById('square-9');
+
+function takeTurn() {
+    if (event.target.innerHTML === '' && playerOne === true) {
+        event.target.innerHTML = 'O';
+        playerOne = !playerOne
+    } else if (event.target.innerHTML === '' && playerOne === false) {
+        event.target.innerHTML = 'X';
+        playerOne = !playerOne
+    }
+    switch (true) {
+        case s1.innerHTML + s2.innerHTML + s3.innerHTML === 'OOO':
+        case s4.innerHTML + s5.innerHTML + s6.innerHTML === 'OOO':
+        case s7.innerHTML + s8.innerHTML + s9.innerHTML === 'OOO':
+        case s1.innerHTML + s4.innerHTML + s7.innerHTML === 'OOO':
+        case s2.innerHTML + s5.innerHTML + s8.innerHTML === 'OOO':
+        case s3.innerHTML + s6.innerHTML + s9.innerHTML === 'OOO':
+        case s1.innerHTML + s5.innerHTML + s9.innerHTML === 'OOO':
+        case s3.innerHTML + s5.innerHTML + s7.innerHTML === 'OOO':
+            alert('Player 1 Wins!');
+            resetGame();
+            document.getElementById('score-1').innerHTML++;
+    }
+    switch (true) {
+        case s1.innerHTML + s2.innerHTML + s3.innerHTML === 'XXX':
+        case s4.innerHTML + s5.innerHTML + s6.innerHTML === 'XXX':
+        case s7.innerHTML + s8.innerHTML + s9.innerHTML === 'XXX':
+        case s1.innerHTML + s4.innerHTML + s7.innerHTML === 'XXX':
+        case s2.innerHTML + s5.innerHTML + s8.innerHTML === 'XXX':
+        case s3.innerHTML + s6.innerHTML + s9.innerHTML === 'XXX':
+        case s1.innerHTML + s5.innerHTML + s9.innerHTML === 'XXX':
+        case s3.innerHTML + s5.innerHTML + s7.innerHTML === 'XXX':
+            alert('Player 2 Wins!');
+            resetGame();
+            document.getElementById('score-2').innerHTML++;
+    }
+};
+
+function resetGame() {
+    playerOne = true;
+    let test = document.getElementsByClassName('squares');
+    Array.from(test).forEach((square) => {
+        square.innerHTML = '';
+    });
+};
