d7b2fcf792dc1011aac8459165806b08cbbe8003
Michael Tew <mike.tew@gmail.com>
2019-07-31 04:13:04 -0700

    Finished the stopwatch

--- scripts/stopwatch.js
+++ scripts/stopwatch.js
@@ -1,77 +1,57 @@
-// Testing section
-let test1 = 0;
-let test2 = 0;
-let test3 = 0;
-
 $(() => {
   $('#navbar').load('navbar.html');
 });
 
-// Declaring timing intervals
-let intMilliseconds = 0;
-let intSeconds = 0;
-let intMinutes = 0;
-let intHours = 0;
-
-// Declaring HTML variables
-const milliseconds = document.getElementById('milliseconds');
-const seconds = document.getElementById('seconds');
-const minutes = document.getElementById('minutes');
-const hours = document.getElementById('hours');
+// Declaring variables.
 let timer;
+let display;
+let centiseconds;
+let seconds;
+let minutes;
+let hours;
+let now = 0;
+let interval = 0;
+let difference = 0;
 
-// Function that stops the timer
+// Function that stops the timer.
 function stopTimer() {
   if (timer) clearInterval(timer);
   timer = null;
 }
 
-// Function that starts the timer
+function start() {
+  interval = Date.now();
+  difference = (interval - now);
+  centiseconds = Math.floor(difference / 10) % 100;
+  seconds = Math.floor(difference / 100 / 10) % 60;
+  minutes = Math.floor(difference / 100 / 60 / 10) % 60;
+  hours = Math.floor(difference / 100 / 60 / 60 / 10);
+
+  // Padding numbers to 2 digits.
+  centiseconds = (centiseconds < 10 ? `0${centiseconds}` : centiseconds);
+  seconds = (seconds < 10 ? `0${seconds}` : seconds);
+  minutes = (minutes < 10 ? `0${minutes}` : minutes);
+  display = `${hours}:${minutes}:${seconds}:${centiseconds}`;
+  document.getElementById('timer').innerHTML = display;
+}
+
+// Function that starts the timer.
 function startTimer() {
+  now = Date.now() - difference;
   stopTimer();
   timer = setInterval(start, 10);
 }
 
-function start() {
-  test1 = Date.now();
-  // console.log(test3);
-  if (intMinutes === 60) {
-    intMinutes = 0;
-    intHours += 1;
-    hours.innerHTML = `${intHours < 10 ? `0${
-      intHours}` : intHours}:`;
-    minutes.innerHTML = `0${intMinutes}:`;
-  } else if (intSeconds === 60) {
-    intSeconds = 0;
-    intMinutes += 1;
-    minutes.innerHTML = `${intMinutes < 10 ? `0${intMinutes}` : intMinutes}:`;
-    seconds.innerHTML = `0${intSeconds}.`;
-  } else if (intMilliseconds >= 100) {
-    test3 = 0;
-    intMilliseconds = 0;
-    intSeconds += 1;
-    seconds.innerHTML = `${intSeconds < 10 ? `0${
-      intSeconds}` : intSeconds}.`;
-  } else {
-    intMilliseconds = test3;
-    milliseconds.innerHTML = (intMilliseconds < 10 ? `0${
-      intMilliseconds}` : intMilliseconds);
-  }
-  test2 = Date.now();
-  test3 += test2 - test1;
-}
-
-document.getElementById('start').addEventListener('click', startTimer);
-document.getElementById('stop').addEventListener('click', stopTimer);
-document.getElementById('reset').addEventListener('click', () => {
+// Function that resets the timer.
+function reset() {
   clearInterval(timer);
+  document.getElementById('timer').innerHTML = '0:00:00:00';
   timer = null;
-  intMilliseconds = 0;
-  intSeconds = 0;
-  intMinutes = 0;
-  intHours = 0;
-  milliseconds.innerHTML = '00';
-  seconds.innerHTML = '00.';
-  minutes.innerHTML = '00:';
-  hours.innerHTML = '00:';
-});
+  now = 0;
+  interval = 0;
+  difference = 0;
+  centiseconds = 0;
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+}
