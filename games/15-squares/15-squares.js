714c5b734fb700b94436e9bbd97ee84ab7029822
Michael Tew <mike.tew@gmail.com>
2022-07-11 23:36:58 -0700

    Working on drawing canvas.

---
+++ games/15-squares/15-squares.js
@@ -0,0 +1,61 @@
+const canvas = document.getElementById('game-canvas')
+const ctx = canvas.getContext('2d')
+const LENGTH = 500
+const SQUARE_LEN = LENGTH / 5
+canvas.width = LENGTH
+canvas.height = LENGTH
+
+ctx.lineWidth = 10
+
+// ================= Square Objects ================
+class Square {
+  constructor(val, x, y) {
+    this.val = val
+    this.x = x
+    this.y = y
+  }
+
+  draw() {
+    ctx.strokeRect(this.x, this.y, SQUARE_LEN, SQUARE_LEN)
+  }
+}
+
+const squareNums = [
+  ' ',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  '11',
+  '12',
+  '13',
+  '14',
+  '15'
+]
+
+const board = []
+while (squareNums.length > 0) {
+  let row = []
+  while (row.length < 4) {
+    x = row.length * SQUARE_LEN
+    y = board.length * SQUARE_LEN
+    square = new Square(squareNums.pop(), x, y)
+    row.push(square)
+  }
+  board.push(row)
+}
+
+console.log(board);
+board.forEach(row => {
+
+})
+
+squares.forEach((square) => {
+  square.draw()
+})
