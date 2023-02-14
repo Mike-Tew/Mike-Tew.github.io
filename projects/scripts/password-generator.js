ac410023985abf4d023b1cba4914f2711068404e
Michael Tew <mike.tew@gmail.com>
2023-02-14 06:10:15 -0800

    Initial slider and checkboxes setup.

--- projects/scripts/password-generator.js
+++ projects/scripts/password-generator.js
@@ -1,1 +1,11 @@
-console.log('object');
+let password = ''
+const slider = document.getElementById('length-slider')
+const lowercase = document.getElementById('lower-check')
+console.log(lowercase.checked)
+const sliderFunc = () => {
+
+}
+const generatePassword = () => {
+  console.log('object');
+}
+
