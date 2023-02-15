979b0854cff5b6d5b7ece0f59c5214b236aff098
Michael Tew <mike.tew@gmail.com>
2023-02-14 23:38:20 -0800

    Updated Password Generator.

--- projects/scripts/password-generator.js
+++ projects/scripts/password-generator.js
@@ -18,18 +18,19 @@ const generatePassword = () => {
   if (uppercase.checked) characterList += characters.uppercase
   if (numbers.checked) characterList += characters.numbers
   if (symbols.checked) characterList += characters.symbols
+  if (!characterList) return password.innerHTML = '(none)'
 
   const passwordLength = slider.value
-  let password = ''
-  while (password.length < slider.value) {
-    password += characterList.charAt(
+  let newPassword = ''
+  while (newPassword.length < slider.value) {
+    newPassword += characterList.charAt(
       Math.floor(Math.random() * characterList.length)
     )
   }
-  console.log(password)
-  return password
+  console.log(newPassword)
+  password.innerHTML = newPassword
 }
 
 const sliderFunc = () => {}
 
-password.innerHTML = generatePassword()
+generatePassword()
