c4c34b3330f8c07707d97fa477e1a5ba225f775e
Michael Tew <mike.tew@gmail.com>
2023-02-14 20:29:46 -0800

    Implemented password generation function.

--- projects/scripts/password-generator.js
+++ projects/scripts/password-generator.js
@@ -1,5 +1,6 @@
 const password = document.getElementById('password')
 const slider = document.getElementById('length-slider')
+const uppercase = document.getElementById('upper-check')
 const lowercase = document.getElementById('lower-check')
 const numbers = document.getElementById('numbers-check')
 const symbols = document.getElementById('symbols-check')
@@ -8,15 +9,27 @@ const characters = {
   lowercase: 'abcdefghijklmnopqrstuvwxyz',
   uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
   numbers: '0123456789',
-  symbols: '!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~'
+  symbols: '!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~'
 }
 
-console.log(lowercase.checked)
 const generatePassword = () => {
-  passwordLength = slider.value
-  password.innerHTML = '123'
+  let characterList = ''
+  if (lowercase.checked) characterList += characters.lowercase
+  if (uppercase.checked) characterList += characters.uppercase
+  if (numbers.checked) characterList += characters.numbers
+  if (symbols.checked) characterList += characters.symbols
+
+  const passwordLength = slider.value
+  let password = ''
+  while (password.length < slider.value) {
+    password += characterList.charAt(
+      Math.floor(Math.random() * characterList.length)
+    )
+  }
+  console.log(password)
+  return password
 }
 
 const sliderFunc = () => {}
 
-generatePassword()
+password.innerHTML = generatePassword()
