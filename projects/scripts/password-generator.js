12f05f8197f255b329dd77efd445c1a65bc8cadb
Michael Tew <mike.tew@gmail.com>
2023-02-14 20:11:13 -0800

    Character object setup.

--- projects/scripts/password-generator.js
+++ projects/scripts/password-generator.js
@@ -1,11 +1,22 @@
-let password = ''
+const password = document.getElementById('password')
 const slider = document.getElementById('length-slider')
 const lowercase = document.getElementById('lower-check')
+const numbers = document.getElementById('numbers-check')
+const symbols = document.getElementById('symbols-check')
+
+const characters = {
+  lowercase: 'abcdefghijklmnopqrstuvwxyz',
+  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  numbers: '0123456789',
+  symbols: '!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~'
+}
+
 console.log(lowercase.checked)
-const sliderFunc = () => {
-
-}
 const generatePassword = () => {
-  console.log('object');
+  passwordLength = slider.value
+  password.innerHTML = '123'
 }
 
+const sliderFunc = () => {}
+
+generatePassword()
