957d737212538c0b51867d2b698d8d22aae3ccac
Michael Tew <mike.tew@gmail.com>
2023-02-15 10:39:41 -0800

    Added event listeners to Generate Password.

--- projects/scripts/password-generator.js
+++ projects/scripts/password-generator.js
@@ -1,9 +1,20 @@
 const password = document.getElementById('password')
+const passwordLength = document.getElementById('password-length')
 const slider = document.getElementById('length-slider')
 const uppercase = document.getElementById('upper-check')
 const lowercase = document.getElementById('lower-check')
 const numbers = document.getElementById('numbers-check')
 const symbols = document.getElementById('symbols-check')
+
+passwordLength.addEventListener('input', () => {
+  slider.value = passwordLength.value
+  generatePassword()
+})
+
+slider.addEventListener('input', () => {
+  passwordLength.value = slider.value
+  generatePassword()
+})
 
 const characters = {
   lowercase: 'abcdefghijklmnopqrstuvwxyz',
@@ -18,19 +29,16 @@ const generatePassword = () => {
   if (uppercase.checked) characterList += characters.uppercase
   if (numbers.checked) characterList += characters.numbers
   if (symbols.checked) characterList += characters.symbols
-  if (!characterList) return password.innerHTML = '(none)'
+  if (!characterList) return (password.innerHTML = '(none)')
 
-  const passwordLength = slider.value
   let newPassword = ''
   while (newPassword.length < slider.value) {
     newPassword += characterList.charAt(
       Math.floor(Math.random() * characterList.length)
     )
   }
-  console.log(newPassword)
+
   password.innerHTML = newPassword
 }
-
-const sliderFunc = () => {}
 
 generatePassword()
