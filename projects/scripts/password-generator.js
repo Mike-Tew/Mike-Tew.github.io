e3c393f87cf80e39379ff6816deeac015a7f5445
Michael Tew <mike.tew@gmail.com>
2023-02-19 13:18:15 -0800

    WIP: Implementing password strength styling.

--- projects/scripts/password-generator.js
+++ projects/scripts/password-generator.js
@@ -6,6 +6,13 @@ const lowercase = document.getElementById('lower-check')
 const numbers = document.getElementById('numbers-check')
 const symbols = document.getElementById('symbols-check')
 
+const characters = {
+  lowercase: 'abcdefghijklmnopqrstuvwxyz',
+  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  numbers: '0123456789',
+  symbols: '!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~'
+}
+
 passwordLength.addEventListener('input', () => {
   if (passwordLength.value > 20) passwordLength.value = 20
   slider.value = passwordLength.value
@@ -16,13 +23,6 @@ slider.addEventListener('input', () => {
   passwordLength.value = slider.value
   generatePassword()
 })
-
-const characters = {
-  lowercase: 'abcdefghijklmnopqrstuvwxyz',
-  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  numbers: '0123456789',
-  symbols: '!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~'
-}
 
 const generatePassword = () => {
   let characterList = ''
@@ -40,10 +40,43 @@ const generatePassword = () => {
   }
 
   password.innerHTML = newPassword
+  changePwdStr()
 }
 
 const copyToClipboard = () => {
   navigator.clipboard.writeText(password.innerHTML)
 }
+
+const changePwdStr = () => {
+  const pwdBackground = document.getElementById('pwd-background')
+  const pwdStrength = document.getElementById('pwd-strength')
+  pwdStrength.innerHTML = "Weak"
+  pwdStrength.style.color = "red"
+  if (slider.value < 7) {
+    pwdBackground.style.backgroundColor = '#eddbdb'
+    pwdBackground.style.borderBottomColor = '#d32d27'
+    pwdStrength.style.color = '#d32d27'
+    pwdStrength.innerHTML = 'Weak'
+
+  } else if (slider.value < 9) {
+    pwdBackground.style.backgroundColor = '#f1edd8'
+    pwdBackground.style.borderBottomColor = '#edbf0e'
+    pwdStrength.style.color = '#edbf0e'
+    pwdStrength.innerHTML = 'Average'
+  } else if (slider.value < 12) {
+    pwdBackground.style.backgroundColor = '#d3eae5'
+    pwdBackground.style.borderBottomColor = '#00a878'
+    pwdStrength.style.color = '#00a878'
+    pwdStrength.innerHTML = 'Strong'
+  } else {
+    pwdBackground.style.backgroundColor = '#d3eae5'
+    pwdBackground.style.borderBottomColor = '#00a878'
+    pwdStrength.style.color = '#00a878'
+    pwdStrength.innerHTML = 'Very strong'
+  }
+  console.log(pwdBackground)
+}
+
+
 
 generatePassword()
