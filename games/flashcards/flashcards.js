da5370cf7825b8964091536cca6eb165dc83ef6d
Michael Tew <mike.tew@gmail.com>
2023-02-24 11:31:34 -0800

    Implemented card shuffling.

--- games/flashcards/flashcards.js
+++ games/flashcards/flashcards.js
@@ -13,17 +13,34 @@ const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const numberCombos = generateNumberCombos(nums)
 
 class Card {
-  constructor(digit1, digit2) {
+  constructor(digit1, digit2, operation) {
     this.digit1 = digit1
     this.digit2 = digit2
-    this.answer = digit1 + digit2
+    this.operation = operation
+    this.solution = digit1 + digit2
   }
 }
 
-const cards = []
-for (let idx in numberCombos) {
-  ;[digit1, digit2] = numberCombos[idx]
-  cards.push(new Card(digit1, digit2))
+const shuffleCards = (deck) => {
+  for (let i = deck.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1))
+    ;[deck[i], deck[j]] = [deck[j], deck[i]]
+  }
+  return deck
 }
 
-console.log(cards)
+const createDeck = (numberCombos) => {
+  const deck = []
+  for (let idx in numberCombos) {
+    ;[digit1, digit2] = numberCombos[idx]
+    deck.push(new Card(digit1, digit2, 'addition'))
+  }
+  return shuffleCards(deck)
+}
+
+const deck = createDeck(numberCombos)
+
+console.log(deck)
+
+const mainCard = document.getElementById('main-card')
+mainCard.innerHTML = deck[1].solution
