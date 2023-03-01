0868d4dfa183a007e3733704d4dd1c3772fd506a
Michael Tew <mike.tew@gmail.com>
2023-03-01 13:38:19 -0800

    Flashcards: Code refactor.

--- games/flashcards/flashcards.js
+++ games/flashcards/flashcards.js
@@ -34,50 +34,64 @@ const showSolution = (card) => {
   mainCard.innerHTML = card.getSolution()
 }
 
-const saveSettings = () => {
-  operations = []
-  if (additionCheck.checked) operations.push('addition')
-  if (subtractionCheck.checked) operations.push('subtraction')
-  if (multiplicationCheck.checked) operations.push('multiplication')
-  if (divisionCheck.checked) operations.push('division')
-  deck = createDeck(numberCombos, operations)
+class Settings {
+  constructor() {
+    this.operations = []
+    this.colorChoices = document.querySelectorAll('.color-choices div')
+    this.currentColor = 'blue'
 
-  mainCard.classList.remove(
-    'bg-blue-300',
-    'bg-red-300',
-    'bg-yellow-300',
-    'bg-green-300',
-    'hover:bg-blue-200',
-    'hover:bg-red-200',
-    'hover:bg-yellow-200',
-    'hover:bg-green-200'
-  )
+    this.colorChoices.forEach((color) => {
+      color.addEventListener('click', () => {
+        this.updateColorBorders()
+        color.style.borderColor = 'rgb(100 116 139)'
+        this.currentColor = color.getAttribute('name')
+      })
+    })
+  }
 
-  if (currentColor == 'blue')
-    mainCard.classList.add('bg-blue-300', 'hover:bg-blue-200')
-  if (currentColor == 'red')
-    mainCard.classList.add('bg-red-300', 'hover:bg-red-200')
-  if (currentColor == 'yellow')
-    mainCard.classList.add('bg-yellow-300', 'hover:bg-yellow-200')
-  if (currentColor == 'green')
-    mainCard.classList.add('bg-green-300', 'hover:bg-green-200')
+  updateColorBorders = () => {
+    this.colorChoices.forEach((color) => {
+      color.style.borderColor = 'rgb(226 232 240)'
+    })
+  }
+
+  getOperations = () => {
+    this.operations = []
+    if (additionCheck.checked) this.operations.push('addition')
+    if (subtractionCheck.checked) this.operations.push('subtraction')
+    if (multiplicationCheck.checked) this.operations.push('multiplication')
+    if (divisionCheck.checked) this.operations.push('division')
+    deck = createDeck(numberCombos, this.operations)
+  }
+
+  updateDeckColor = () => {
+    mainCard.classList.remove(
+      'bg-blue-300',
+      'bg-red-300',
+      'bg-yellow-300',
+      'bg-green-300',
+      'hover:bg-blue-200',
+      'hover:bg-red-200',
+      'hover:bg-yellow-200',
+      'hover:bg-green-200'
+    )
+
+    if (this.currentColor == 'blue')
+      mainCard.classList.add('bg-blue-300', 'hover:bg-blue-200')
+    if (this.currentColor == 'red')
+      mainCard.classList.add('bg-red-300', 'hover:bg-red-200')
+    if (this.currentColor == 'yellow')
+      mainCard.classList.add('bg-yellow-300', 'hover:bg-yellow-200')
+    if (this.currentColor == 'green')
+      mainCard.classList.add('bg-green-300', 'hover:bg-green-200')
+  }
+
+  save = () => {
+    this.getOperations()
+    this.updateDeckColor()
+  }
 }
 
-saveButton.addEventListener('click', saveSettings)
-
-const deckColor = document.querySelectorAll('.deck-color div')
-let currentColor
-
-const resetColorBorders = () => {
-  deckColor.forEach((color) => {
-    color.style.borderColor = 'rgb(226 232 240)'
-  })
-}
-
-deckColor.forEach((color) => {
-  color.addEventListener('click', () => {
-    resetColorBorders()
-    color.style.borderColor = 'rgb(100 116 139)'
-    currentColor = color.getAttribute('name')
-  })
-})
+// saveButton.addEventListener('click', saveSettings)
+const settings = new Settings()
+saveButton.addEventListener('click', settings.save)
