c456c2d940edd26e042449a84039e96a30dea011
Michael Tew <mike.tew@gmail.com>
2023-07-03 21:26:15 -0700

    Code refactor.

--- projects/scripts/reaction-time.js
+++ projects/scripts/reaction-time.js
@@ -1,39 +1,42 @@
-let startTime;
-let endTime;
-let randNum;
-let reactionTime;
-let delay;
-const clickySquare = document.getElementById('container');
-const color = 'rgb(228, 228, 228)';
-clickySquare.style.backgroundColor = color;
+let startTime
+let endTime
+let randNum
+let reactionTime
+let delay
+const clickSquare = document.getElementById('click-container')
+const DEFAULT_COLOR = 'rgb(228, 228, 228)'
+clickSquare.style.backgroundColor = DEFAULT_COLOR
 
-clickySquare.addEventListener('click', () => {
-  if (clickySquare.style.backgroundColor === color) {
-    clickySquare.children[0].innerHTML = '';
-    clickySquare.style.backgroundColor = 'red';
-    randNum = (Math.random() * (5 - 3) + 3) * 1000;
+clickSquare.addEventListener('click', () => {
+  if (clickSquare.style.backgroundColor === DEFAULT_COLOR) {
+    clickSquare.children[0].innerHTML = ''
+    clickSquare.style.backgroundColor = 'red'
+    randNum = (Math.random() * (5 - 3) + 3) * 1000
     delay = setTimeout(() => {
-      clickySquare.style.backgroundColor = 'green';
-    }, randNum);
-    startTime = new Date().getTime();
-  } else if (clickySquare.style.backgroundColor === 'green') {
-    endTime = new Date().getTime();
-    reactionTime = Math.floor(endTime - startTime - randNum) / 1000;
-    document.getElementById('reaction-time')
-      .innerHTML = `${reactionTime} Seconds.`;
-    clickySquare.style.backgroundColor = 'blue';
-  } else if (clickySquare.style.backgroundColor === 'red') {
-    clickySquare.children[0].innerHTML = 'You clicked too soon!';
-    clearTimeout(delay);
+      clickSquare.style.backgroundColor = 'green'
+    }, randNum)
+    startTime = new Date().getTime()
+  } else if (clickSquare.style.backgroundColor === 'green') {
+    endTime = new Date().getTime()
+    reactionTime = Math.floor(endTime - startTime - randNum) / 1000
+    document.getElementById(
+      'reaction-time'
+    ).innerHTML = `${reactionTime} Seconds.`
+    clickSquare.style.backgroundColor = 'blue'
+  } else if (clickSquare.style.backgroundColor === 'red') {
+    clickSquare.children[0].innerHTML = 'You clicked too soon!'
+    clearTimeout(delay)
   }
-});
+})
 
 function reset() {
-  document.getElementById('reaction-time').innerHTML = '';
-  clickySquare.style.backgroundColor = color;
-  clickySquare.children[0].innerHTML = 'Click to start';
-  startTime = 0;
-  endTime = 0;
-  reactionTime = 0;
-  randNum = 0;
+  document.getElementById('reaction-time').innerHTML = ''
+  clickSquare.style.backgroundColor = DEFAULT_COLOR
+  clickSquare.children[0].innerHTML = 'Click To Start'
+  startTime = 0
+  endTime = 0
+  reactionTime = 0
+  randNum = 0
 }
+
+document.getElementById('reset-button').addEventListener('click', reset)
