d97174923324dd0a10b3fe5c2c4d6b5742324832
Michael Tew <mike.tew@gmail.com>
2023-10-02 12:42:34 -0700

    Blackjack: Improved styling.

--- games/card-games/script.js
+++ games/card-games/script.js
@@ -6,6 +6,9 @@ const dealerArea = document.getElementById('dealer-area')
 const deck = new Deck()
 const playerHand = []
 const dealerHand = []
+let playerScore = 0
+let dealerScore = 0
+let playerFinished = false
 
 const showCard = (area, card) => {
   const img = document.createElement('img')
@@ -52,18 +55,42 @@ const getHandScore = (hand) => {
 }
 
 const checkScores = () => {
-  let playerScore = getHandScore(playerHand)
-  let dealerScore = getHandScore(dealerHand)
-  console.log(playerScore, dealerScore);
+  playerScore = getHandScore(playerHand)
+  dealerScore = getHandScore(dealerHand)
+  console.log(playerScore, dealerScore)
+
+  if (playerScore == 21) playerWin()
+  if (playerScore > 21) playerLose()
+}
+
+const hit = (hand, area) => {
+  const card = deck.cards.shift()
+  hand.push(card)
+  showCard(area, card)
+  checkScores()
+}
+
+const dealerPlay = () => {
+  playerFinished = true
+  while (dealerScore < 17) {
+    hit(dealerHand, dealerArea)
+  }
+}
+
+const playerWin = () => {
+  console.log('You Win!');
+}
+
+const playerLose = () => {
+  playerFinished = true
+  console.log('Bust!');
 }
 
 gameStart()
 
-document.getElementById('next').addEventListener('click', () => {
-  const card = deck.cards.shift()
-  console.log(card)
-  playerHand.push(card)
-  showCard(playerArea, card)
-  console.log(playerHand)
-  checkScores()
+document.getElementById('hit-button').addEventListener('click', () => {
+  if (playerFinished) return
+
+  hit(playerHand, playerArea)
 })
+document.getElementById('stay-button').addEventListener('click', dealerPlay)
