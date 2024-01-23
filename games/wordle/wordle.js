9586f52f33a8464ee26f2d208945f9aeebbea4ab
Michael Tew <mike.tew@gmail.com>
2024-01-23 08:34:26 -0800

    Wordle keyboard building.

--- games/wordle/wordle.js
+++ games/wordle/wordle.js
@@ -1,15 +1,12 @@
 const keyboard = document.getElementById('keyboard')
-LETTERS = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P']
+const LETTERS = 'QWERTYUIOPASDFGHJKLZXCVBNM'
 
-// keyboard.appendChild(keyEl)
 const createEle = () => {
-  console.log('object');
-  const keyEl = document.createElement('p')
-  // keyEl.innerText = 'sdlfkj'
-  const txt = document.createTextNode('123')
-  // keyboard.appendChild(txt)
-  document.getElementById('keyboard').appendChild(txt)
-
+  LETTERS.split('').forEach((letter) => {
+    keyboard.innerHTML += `
+    <div class="key">${letter}</div>
+    `
+  })
 }
 
 document.body.onload = createEle
