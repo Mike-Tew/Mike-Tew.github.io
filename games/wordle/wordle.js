c42e9ad1bc79f6ff6f7062cd1464f0110c547cd2
Michael Tew <mike.tew@gmail.com>
2024-01-23 20:32:03 -0800

    Wordle: working on game functionality.

--- games/wordle/wordle.js
+++ games/wordle/wordle.js
@@ -1,5 +1,8 @@
 const keyboard = document.getElementById('keyboard')
-const LETTERS = ['QWERTYUIOP', 'ASDFGHJKL', 'ZXCVBNM']
+const LETTERS = ['QWERTYUIOP', 'ASDFGHJKL', '1ZXCVBNM0']
+let turn = 1
+let currentGuess = []
+let word = 'PARTY'
 
 const createKeyboard = () => {
   LETTERS.forEach((row) => {
@@ -7,9 +10,32 @@ const createKeyboard = () => {
     keyRow.classList.add('row')
     keyboard.appendChild(keyRow)
     row.split('').forEach((letter) => {
-        keyRow.innerHTML += `<div class="key">${letter}</div>`
+      const keyEl = document.createElement('div')
+      keyEl.classList.add('key')
+      keyEl.innerText = letter
+      keyEl.onclick = chooseLetter
+      keyRow.appendChild(keyEl)
     })
   })
+}
+
+const chooseLetter = (e) => {
+  const letter = e.target.textContent
+  if (letter === '0') {
+    currentGuess.pop()
+  } else if (letter === '1') {
+    checkGuess()
+  } else if (currentGuess.length < 6) {
+    currentGuess.push(letter)
+  }
+  console.log(currentGuess, currentGuess.length)
+}
+
+const checkGuess = () => {
+  console.log(currentGuess.join(''), word)
+  if (currentGuess.join('') === word) {
+    console.log('You Win!')
+  }
 }
 
 document.body.onload = createKeyboard
